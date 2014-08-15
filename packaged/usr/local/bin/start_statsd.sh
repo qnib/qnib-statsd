250f92a72549513fd536b74ce0c53b3e0dfa0867
Christian Kniep <christian@qnib.org>
2014-08-15 11:49:02 +0200

    supervisord script and start_script that adjusts config

---
+++ packaged/usr/local/bin/start_statsd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CARBON_IP=${CARBON_PORT_2003_TCP_ADDR}
+if [ "X${CARBON_IP}" == "X" ];then
+   CARBON_IP=$(dig +short carbon)
+   if [ "X${CARBON_IP}" == "X" ];then
+       echo "ERROR: Carbon could not be resolved"
+       exit 1
+   fi
+fi
+cat /etc/statsd/config.js.example |sed -e "s/, graphiteHost: .*/, graphiteHost: ${CARBON_IP}/" > /etc/statsd/config.js
+
+# start statsd
+/bin/node /usr/local/statsd/stats.js /etc/statsd/config.js
